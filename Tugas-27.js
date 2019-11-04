1535261c17ca4522e48fc971e7ad8d40cae99d94
Qansa Alghifari <46017824+Qansa2916@users.noreply.github.com>
2019-11-04 10:59:47 +0700

    Tugas_27_JS

---
+++ Tugas-27.js
@@ -0,0 +1,14 @@
+function tugas27() {
+    var a = 'No ';
+    var b = 'rumah saya ';
+    var c = '30 ';
+    var d = 'A';
+    e = a + b + c + d;
+    console.log(e);
+    var x = 'Jalan pangeran Tirtayasa No '
+    var y = 70;
+    z = x + String(y);
+    console.log(z);
+
+}
+tugas27();
